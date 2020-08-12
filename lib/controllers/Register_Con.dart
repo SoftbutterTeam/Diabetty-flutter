27c3ec7aab28a258ed28bfdcdbb7649f058f1604
emcleandev <58271203+emcleandev@users.noreply.github.com>
2020-08-12 21:32:21 +0100

    ^ register Code

--- lib/controllers/Register_Con.dart
+++ lib/controllers/Register_Con.dart
@@ -1,11 +1,21 @@
 import 'package:diabetttty/models/AppData.dart';
+import 'package:diabetttty/models/UserAccount.dart';
 
 class RegisterCon {
-  static bool registerAsGuest(AppState appState) {
-    if (appState.userAccount.type != null) {
-      appState.userAccount.initGuestUser();
+  static bool registerAsGuest(AppState appState, String name) {
+    if (appState.userAccount.loggedIn != true) {
+      appState.userAccount.initGuestUser(name: name);
       return true;
     }
     return false;
+  }
+
+  static bool registerUserA(AppState appState, RegisterUserForm registerInfo) {
+    if (appState.userAccount.loggedIn != true) {
+      appState.userAccount.registerUserA(registerInfo);
+    }
+
+    // TODO create a model for Form info, pass it through. Then edit registerUserA to extract what is can.
+    // TODO wrtie saveData() after creation
   }
 }
