3a4cebee9f0c2e69c61556f4e2c78cd43f68228f
Cameron MacKay <40600212+CMackay7@users.noreply.github.com>
2020-09-24 15:51:46 +0100

    added models
    medication info model and therapy model

--- lib/models/theraphy.model.dart
+++ lib/models/theraphy.model.dart
@@ -1,17 +1,19 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:diabetttty/models/MedicationInfo.dart';
 
 class Theraphy {
   String uid;
   String name;
-}
-
-class MedicationInfo {
-  String name;
+  Schedule schedule;
+  MedicationInfo medicationInfo;
 }
 
 class Schedule {
   List<Reminders> reminders;
   ScheduleSettings settings;
+  Schedule({
+    this.reminders,
+    this.settings,
+  });
 }
 
 class Reminders {
@@ -19,12 +21,29 @@ class Reminders {
   double dose;
   String time;
   String window;
+  Reminders({
+    this.days,
+    this.dose,
+    this.time,
+    this.window,
+  });
 }
 
 class Days {
-  bool monday, tuesday, wednesday, thursday, friday, saturaday, sunday;
+  bool monday, tuesday, wednesday, thursday, friday, saturday, sunday;
+  Days(bool monday, bool tuesday, bool, wednesday, bool thursday, bool friday,
+      bool saturday, bool sunday) {
+    if (monday != null) this.monday = monday;
+    if (tuesday != null) this.tuesday = tuesday;
+    if (wednesday != null) this.wednesday = wednesday;
+    if (thursday != null) this.thursday = thursday;
+    if (friday != null) this.friday = friday;
+    if (saturday != null) this.saturday = saturday;
+    if (sunday != null) this.sunday = sunday;
+  }
 }
 
 class ScheduleSettings {
   bool silent = false;
+  ScheduleSettings({this.silent});
 }
