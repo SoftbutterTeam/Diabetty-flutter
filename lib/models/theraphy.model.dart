0293443d5fae54cd8b47198c73363ecd9fcc4771
emcleandev <58271203+emcleandev@users.noreply.github.com>
2020-09-24 21:30:43 +0800

    +

---
+++ lib/models/theraphy.model.dart
@@ -0,0 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Theraphy {
+  String uid;
+  String type;
+  MedicationInfo medicationInfo;
+  String email;
+  String phoneNumber;
+}
+
+class MedicationInfo {
+  String name;
+}
