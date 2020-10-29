c45897c65e15a48a9f091d0912b63ec28e61f5f3
emcleandev <58271203+emcleandev@users.noreply.github.com>
2020-10-29 17:56:42 +0000

    + refactoring

--- lib/ui/screens/therapy/components/index.dart
+++ lib/ui/screens/therapy/components/index.dart
@@ -2,7 +2,7 @@ export 'AppearancePopUp.dart';
 export 'CustomTextField.dart';
 export 'InputTextField.dart';
 export 'medication_card.dart';
-export 'MinRestPopUp.dart';
+export 'custom_timer_picker.dart';
 export 'ModePopUp.dart';
 export 'placeholder.dart';
 export 'StrengthDialog.dart';
