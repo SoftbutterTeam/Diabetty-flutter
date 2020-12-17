1904e2161ed20cdb0d7e83e3c6ceb46791899fe6
emcleandev <58271203+emcleandev@users.noreply.github.com>
2020-12-17 18:48:15 +0000

    +

--- lib/ui/screens/diary/a_journal/journal_add_record.modal.dart
+++ lib/ui/screens/diary/a_journal/journal_add_record.modal.dart
@@ -196,6 +196,7 @@ class _JournalAddRecordState extends State<JournalAddRecord> {
                       double.parse(inputController.text);
                   Provider.of<DiaryBloc>(context, listen: false)
                       .saveJournalEntry(journalRecord);
+                  Navigator.of(context).pop(context);
                   //_handleSubmit();
                 }
               },
